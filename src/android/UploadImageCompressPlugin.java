f9092bdd543b1c0833e3007e3506d1c06172c927
nombx <nombx@nombxdeMac-Pro.local>
2015-06-28 18:02:48 +0000

    renamed:    src/android/upload-image-compress-plugin.java -> src/android/UploadImageCompressPlugin.java

--- src/android/UploadImageCompressPlugin.java
+++ src/android/UploadImageCompressPlugin.java
@@ -10,7 +10,7 @@ import org.json.JSONObject;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class upload-image-compress-plugin extends CordovaPlugin {
+public class UploadImageCompressPlugin extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
