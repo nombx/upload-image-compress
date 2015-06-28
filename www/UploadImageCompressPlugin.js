61efb3b1509ce2bfa915bae3b33b48c2eab0301d
nombx <luan.nombx@gmail.com>
2015-06-28 10:37:12 +0000

    Update UploadImageCompressPlugin.js

--- www/UploadImageCompressPlugin.js
+++ www/UploadImageCompressPlugin.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "upload-image-compress-plugin", "coolMethod", [arg0]);
+    exec(success, error, "UploadImageCompressPlugin", "coolMethod", [arg0]);
 };
